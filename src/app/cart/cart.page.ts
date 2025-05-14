ae0dadcbcbb83f30a3e212dd8ef4963324f0f3d7
Fabian Mella Barra <fa.mella@duocuc.cl>
2025-05-14 13:58:52 -0400

    Modify standalone
    Modify standalone in differents typescripts due to the design of the page.

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -4,6 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-cart',
   templateUrl: './cart.page.html',
   styleUrls: ['./cart.page.scss'],
+  standalone: false
+  // Note: The 'standalone' property is used in Angular to indicate that this component is not part of any module.
 })
 export class CartPage implements OnInit {
 
--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -4,6 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-products',
   templateUrl: './products.page.html',
   styleUrls: ['./products.page.scss'],
+  standalone: false
+  // Note: The 'standalone' property is used in Angular to indicate that this component is not part of any module.
 })
 export class ProductsPage implements OnInit {
 
