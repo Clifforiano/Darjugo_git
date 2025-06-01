c121db2f7133b0306576d07277281de6c55ac509
Fabian Mella Barra <fa.mella@duocuc.cl>
2025-05-31 22:37:14 -0400

    Models order

--- src/app/shared/components/orders/order-tracking/order-tracking.component.ts
+++ src/app/shared/components/orders/order-tracking/order-tracking.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// Update the import path below to the correct location of order.model.ts
+// Update the path below to the correct location of order.model.ts
+// Update the path below to the correct location of order.model.ts
+import { OrderModule } from '../../../models/order.module'; // Adjust the import path as necessarys
+
 
 @Component({
+  standalone: true,
   selector: 'app-order-tracking',
   templateUrl: './order-tracking.component.html',
   styleUrls: ['./order-tracking.component.scss'],
+  imports: [CommonModule]
 })
-export class OrderTrackingComponent  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class OrderTrackingComponent {
+  @Input() order!: OrderModule;
+  statusSteps = [
+    { id: 'pendiente', label: 'Pendiente' },
+    { id: 'procesando', label: 'Procesando' },
+    { id: 'enviado', label: 'Enviado' },
+    { id: 'entregado', label: 'Entregado' }
+  ];
 }
