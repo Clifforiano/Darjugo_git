0be785fcc72f5fe9fee260360ae4cbc9f16b943f
Clifforiano <135030191+Clifforiano@users.noreply.github.com>
2025-06-19 17:19:16 -0400

    implementaciones finales

---
+++ src/app/shared/components/forgot-reser-password/forgot-reser-password.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-reser-password',
+  templateUrl: './forgot-reser-password.component.html',
+  styleUrls: ['./forgot-reser-password.component.scss'],
+})
+export class ForgotReserPasswordComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
